8378e7776b3af307ce616cba162b01f9517e7331
yurginapoet <yurginapoet@gmail.com>
2023-03-02 02:37:53 +0700

    added py generator

---
+++ generator/generator.py
@@ -0,0 +1,6 @@
+with open("input.txt", "w") as output:
+
+    for i in range(1, 1001):
+        output.write(str(i)+' ')
+
+    output.close()
--- generator/numbers_generator.py
+++
@@ -1,4 +0,0 @@
-file = open('out.txt', 'w')
-
-for i in range(1, 101):
-    print(i)
